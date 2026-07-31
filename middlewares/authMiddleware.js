71239c747bff130109e17ca47bb17ff1f4083ec1
BostHost <lopezdavison7@gmail.com>
2026-07-31 12:49:11 -0600

    Crear middlewares/authMiddleware.js

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,52 @@
+const jwt = require("jsonwebtoken");
+
+/*
+==================================
+ LUNARA AUTH MIDDLEWARE
+ Creado por Luis González
+==================================
+*/
+
+const authMiddleware = (req, res, next) => {
+
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({
+            success: false,
+            message: "Token no proporcionado."
+        });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).json({
+            success: false,
+            message: "Token inválido."
+        });
+    }
+
+    try {
+
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+
+        req.user = decoded;
+
+        next();
+
+    } catch (error) {
+
+        return res.status(401).json({
+            success: false,
+            message: "Token expirado o inválido."
+        });
+
+    }
+
+};
+
+module.exports = authMiddleware;
