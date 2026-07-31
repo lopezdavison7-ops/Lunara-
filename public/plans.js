f81474b212a6d65ea2eaa037bac5ba294d1bcd60
BostHost <lopezdavison7@gmail.com>
2026-07-31 14:36:14 -0600

    Crear plans.js

---
+++ public/plans.js
@@ -0,0 +1,41 @@
+/*
+==================================
+ LUNARA PLANS SCRIPT
+ Creado por Luis González
+==================================
+*/
+
+
+const paypalButton = document.getElementById(
+    "paypalButton"
+);
+
+
+
+paypalButton.addEventListener(
+"click",
+() => {
+
+
+    /*
+    ==================================
+    PAYPAL
+
+    Aquí después conectaremos:
+    - PayPal Checkout
+    - Confirmación de pago
+    - Cambio automático a Premium
+
+    La cuenta de PayPal se manejará
+    desde el backend para mantener
+    seguridad.
+    ==================================
+    */
+
+
+    alert(
+        "🚀 Próximamente podrás activar Premium con PayPal."
+    );
+
+
+});
