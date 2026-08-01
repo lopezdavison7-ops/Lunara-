28aced745be873ca70fae65a99d14c8ba40a4c31
BostHost <lopezdavison7@gmail.com>
2026-07-31 18:23:27 -0600

    Actualizar auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,11 @@
 const express = require("express");
-
-const {
-    register,
-    login,
-    profile
-} = require("../controllers/authController");
-
 const router = express.Router();
+
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/User");
+
 
 /*
 ==================================
@@ -15,13 +14,191 @@ const router = express.Router();
 ==================================
 */
 
-// Registro
-router.post("/register", register);
 
-// Inicio de sesión
-router.post("/login", login);
+// REGISTRO
 
-// Perfil del usuario
-router.get("/profile", profile);
+router.post("/register", async(req,res)=>{
+
+    try{
+
+        const {
+            username,
+            email,
+            password
+        } = req.body;
+
+
+        const exists = await User.findOne({
+
+            email
+
+        });
+
+
+        if(exists){
+
+            return res.status(400).json({
+
+                message:"El correo ya está registrado"
+
+            });
+
+        }
+
+
+        const hashedPassword = await bcrypt.hash(
+
+            password,
+
+            10
+
+        );
+
+
+        const user = await User.create({
+
+            username,
+
+            email,
+
+            password:hashedPassword
+
+        });
+
+
+        res.json({
+
+            message:"Usuario creado correctamente"
+
+        });
+
+
+    }catch(error){
+
+
+        res.status(500).json({
+
+            message:"Error creando usuario"
+
+        });
+
+
+    }
+
+});
+
+
+
+// LOGIN
+
+router.post("/login", async(req,res)=>{
+
+    try{
+
+
+        const {
+
+            email,
+
+            password
+
+        } = req.body;
+
+
+
+        const user = await User.findOne({
+
+            email
+
+        });
+
+
+
+        if(!user){
+
+            return res.status(400).json({
+
+                message:"Usuario no encontrado"
+
+            });
+
+        }
+
+
+
+        const validPassword = await bcrypt.compare(
+
+            password,
+
+            user.password
+
+        );
+
+
+
+        if(!validPassword){
+
+            return res.status(400).json({
+
+                message:"Contraseña incorrecta"
+
+            });
+
+        }
+
+
+
+        const token = jwt.sign(
+
+            {
+
+                id:user._id
+
+            },
+
+            process.env.JWT_SECRET,
+
+            {
+
+                expiresIn:"7d"
+
+            }
+
+        );
+
+
+
+        res.json({
+
+            token,
+
+            user:{
+
+                username:user.username,
+
+                email:user.email,
+
+                plan:user.plan
+
+            }
+
+        });
+
+
+
+    }catch(error){
+
+
+        res.status(500).json({
+
+            message:"Error iniciando sesión"
+
+        });
+
+
+    }
+
+});
+
 
 module.exports = router;
